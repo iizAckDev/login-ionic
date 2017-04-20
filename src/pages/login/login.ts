978edf1835c9850eea9c1513fa698e4516f0fd65
iizAckDev <imro.dev@gmail.com>
2017-04-19 23:53:58 -0300

    Login Page
    Composición de la pagina de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,20 +1,65 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { RegisterPage } from '../register';
+import { HomePage } from '../home';
 
-/**
- * Generated class for the Login page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
-export class Login {
+export class LoginPage {
+  loading : Loading;
+  registerCredentials = { email : '', password : '' };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private nav: NavController, 
+    private auth: AuthService, 
+    private alertCtrl: AlertController, 
+    private loadingCtrl: LoadingController
+  ) {}
+ 
+  public createAccount() {
+    this.nav.push(RegisterPage);
+  }
+
+  public login(){
+    this.showLoading();
+    this.auth.login( this.registerCredentials).subscribe(allowed => {
+      if( allowed ){
+        setTimeout( () => {
+          this.loading.dismiss();
+          this.nav.setRoot(HomePage);
+        });
+      } else {
+        this.showError( "Acceso Denegado" );
+      }
+    },
+    error => {
+      this.showError(error);
+    });
+  }
+
+  showLoading(){
+    this.loading = this.loadingCtrl.create({
+      content: 'Por favor espere...'
+    });
+    this.loading.present();
+  }
+
+  showError(text){
+    setTimeout(()=>{
+      this.loading.dismiss();
+    });
+
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subtitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+    
   }
 
   ionViewDidLoad() {
--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -1,12 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+export class User {
+  name  : string;
+  email : string;
+
+  constructor( name: string, email: string ){
+    this.name  = name;
+    this.email = email;
+  }
+}
+
+
 @Injectable()
 export class AuthService {
+  currentUser : User;
 
-  constructor(public http: Http) {
-    console.log('Hello AuthService Provider');
+  public login ( credentials ){
+    if ( credentials.email === null || credentials.password === null ){
+      return Observable.throw("Por favor ingrese su credencial de acceso");
+    }else{
+      return Observable.create( observer => {
+        let access = (credentials.password === "pass" && credentials.email === "email");
+        this.currentUser = new User( "Simon", "simon@email.com");
+        observer.next(access);
+        observer.complete();
+      });
+    }
   }
 
+  public register( credentials ){
+    if ( credentials.email === null || credentials.password === null ){
+      return Observable.throw("Por favor ingrese una credencial");
+    }else{
+      return Observable.create(observer =>{
+        observer.next(true);
+        observer.complete();
+      });
+    }
+  }
+
+  public getUserInfo() : User {
+    return this.currentUser;
+  }
+
+  public logout(){
+    return Observable.create( observer => {
+      this.currentUser = null;
+      observer.next(true);
+      observer.complete();
+    });
+  }
 }
